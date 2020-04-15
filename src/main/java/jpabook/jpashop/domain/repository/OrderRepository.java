8eab547409112fe77f6379a53a46993726426222
kgh940525 <kgh940525@gmail.com>
2020-04-15 15:49:34 +0900

    Add Dynamic Query

--- src/main/java/jpabook/jpashop/domain/repository/OrderRepository.java
+++ src/main/java/jpabook/jpashop/domain/repository/OrderRepository.java
@@ -3,9 +3,13 @@ package jpabook.jpashop.domain.repository;
 import jpabook.jpashop.domain.Order;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Repository;
+import org.springframework.util.StringUtils;
 
 import javax.persistence.EntityManager;
+import javax.persistence.TypedQuery;
+import javax.persistence.criteria.*;
 import javax.swing.text.html.parser.Entity;
+import java.util.ArrayList;
 import java.util.List;
 
 @Repository
@@ -23,7 +27,105 @@ public class OrderRepository {
     }
 
     // 동적쿼리가 들어가야하기 때문에 설명이 필요함
-//    public List<Order> findAll(OrderSearch orderSearch){
-//
-//    }
+    public List<Order> findAll(OrderSearch orderSearch) {
+        /* 무식한 방법 1 */
+
+        String jpql = "select o from Order o join o.member m";
+
+        boolean isFirstCondition = true;
+
+        //주문 상태 검색
+        if (orderSearch.getOrderStatus() != null) {
+            if (isFirstCondition) {
+                jpql += " where";
+                isFirstCondition = false;
+            } else {
+                jpql += " and";
+            }
+            jpql += " o.status = :status";
+        }
+
+        //회원 이름 검색
+        if (StringUtils.hasText(orderSearch.getMemberName())) {
+            if (isFirstCondition) {
+                jpql += " where";
+                isFirstCondition = false;
+            } else {
+                jpql += " and";
+            }
+            jpql += " m.name like :name";
+        }
+
+        TypedQuery<Order> query = em.createQuery(jpql, Order.class)
+                .setMaxResults(1000); //최대 1000건
+        if (orderSearch.getOrderStatus() != null) {
+            query = query.setParameter("status", orderSearch.getOrderStatus());
+        }
+        if (StringUtils.hasText(orderSearch.getMemberName())) {
+            query = query.setParameter("name", orderSearch.getMemberName());
+        }
+        return query.getResultList();
+
+
+
+    }
+
+    /*
+    JPA Criteria
+    조금더 나은방법 표준 방식 동적쿼리
+     */
+    /*
+    public List<Order> findAllByString(OrderSearch orderSearch) {
+        //language=JPAQL
+        String jpql = "select o From Order o join o.member m";
+        boolean isFirstCondition = true;
+        //주문 상태 검색
+        if (orderSearch.getOrderStatus() != null) {
+            if (isFirstCondition) {
+                jpql += " where";
+                isFirstCondition = false;
+            } else {
+                jpql += " and";
+            }
+            jpql += " o.status = :status";
+        }
+        //회원 이름 검색
+        if (StringUtils.hasText(orderSearch.getMemberName())) {
+            if (isFirstCondition) {
+                jpql += " where";
+                isFirstCondition = false;
+            } else {
+                jpql += " and";
+            }
+            jpql += " m.name like :name";
+        }
+        TypedQuery<Order> query = em.createQuery(jpql, Order.class)
+                .setMaxResults(1000); //최대 1000건
+        if (orderSearch.getOrderStatus() != null) {
+            query = query.setParameter("status", orderSearch.getOrderStatus());
+        }
+        if (StringUtils.hasText(orderSearch.getMemberName())) {
+            query = query.setParameter("name", orderSearch.getMemberName());
+        }
+        return query.getResultList();
+    }
+    */
+
+
+
+/*
+        return em.createQuery("select o from Order o join o.member m" +
+                        "where o.status = :status" +
+                        "and m.name like :name", Order.class)
+                        .setParameter("status", orderSearch.getOrderStatus())
+                        .setParameter("name", orderSearch.getMembername())
+                        .setMaxResults(1000) // 최대 1000건
+                        .getResultList();
+
+ */
+
+        // 동적 쿼리를 어떻게 처리할까?
+
+
+    // 페이징도 가능하다 getFirstResult
 }
