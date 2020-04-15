7a5fb7a1d85e03d02785e6f399805d36e5f33315
kgh940525 <kgh940525@gmail.com>
2020-04-15 16:19:57 +0900

    Add Home Layout

---
+++ src/main/java/jpabook/jpashop/controller/HomeController.java
@@ -0,0 +1,24 @@
+package jpabook.jpashop.controller;
+
+import lombok.extern.slf4j.Slf4j;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import sun.rmi.runtime.Log;
+
+@Controller
+@Slf4j
+public class HomeController {
+
+
+    //Logger log = LoggerFactory.getLogger(getClass()); 어노테이션으로 처리할 수 있다.
+
+
+    @RequestMapping("/")
+    public String home(){
+        log.info("home Controller");
+        return "home";          // home.html 파일을 찾아가게된다.
+    }
+
+}
